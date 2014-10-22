b2bb5faf2f42477516ff84f9d49127a4053a1937
Philipp Klaus <philipp.klaus@gmail.com>
2014-10-22 16:38:11 +0200

    preloader: switched to argparse

--- preloader.py
+++ preloader.py
@@ -6,43 +6,45 @@ Preloader for PhotoBrowser
 uses Python3
 """
 
-import sys
-
-try:
-    site = sys.argv[1]
-except:
-    sys.stderr.write("usage: " + sys.argv[0] + " SITE_URL [password]\n")
-    sys.exit(2)
-try:
-    password = sys.argv[2]
-except:
-    password = None
-
 import urllib.request
 import urllib.parse
 import http.cookiejar
 import json
+import sys
+import argparse
 
-print("Logging in")
+def main():
 
-cj = http.cookiejar.CookieJar()
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-urllib.request.install_opener(opener)
-if password:
-    data=[('password',password)] 
-    response = urllib.request.urlopen(urllib.parse.urljoin(site, '/login?requesting=/'), urllib.parse.urlencode(data).encode('utf-8')).read()
-    if "FAILED" in response.decode('utf-8'):
-        print('bad credentials, exiting.')
-        sys.exit(3)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('site_url', metavar="SITE_URL",
+                       help='URL to your PhotoBrowser site.')
+    parser.add_argument('--password', '-p',
+                       help='Provide the password to your site if needed.')
+    args = parser.parse_args()
 
-response = urllib.request.urlopen(urllib.parse.urljoin(site, '/api/list/images')).read().decode('utf-8')
-images = json.loads(response)['images']
+    print("Logging in")
 
-print("Preloading images for " + site)
+    cj = http.cookiejar.CookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    urllib.request.install_opener(opener)
+    if args.password:
+        data=[('password',args.password)]
+        response = urllib.request.urlopen(urllib.parse.urljoin(args.site_url, '/login?requesting=/'), urllib.parse.urlencode(data).encode('utf-8')).read()
+        if "FAILED" in response.decode('utf-8'):
+            print('bad credentials, exiting.')
+            sys.exit(3)
 
-site = urllib.parse.urljoin(site, '/api/image/scaled/600/')
-for image in images:
-    print("Fetching {}.".format(image))
-    image = urllib.parse.quote(image)
-    urllib.request.urlopen(urllib.parse.urljoin(site, image)).read()
+    response = urllib.request.urlopen(urllib.parse.urljoin(args.site_url, '/api/list/images')).read().decode('utf-8')
+    images = json.loads(response)['images']
+
+    print("Preloading images for " + args.site_url)
+
+    base_url= urllib.parse.urljoin(args.site_url, '/api/image/scaled/600/')
+    for image in images:
+        print("Fetching {}.".format(image))
+        image = urllib.parse.quote(image)
+        urllib.request.urlopen(urllib.parse.urljoin(base_url, image)).read()
+
+if __name__ == "__main__":
+    main()
 
